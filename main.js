54b995cff479d70186641b053fab7a3e13bbdce3
hernanrioscondori <hrabbitc@gmail.com>
2022-11-07 16:49:19 -0400

    sub 8

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // initialize MindAR 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: 'tarjets.mind',
+      imageTargetSrc: 'tarjets1.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
